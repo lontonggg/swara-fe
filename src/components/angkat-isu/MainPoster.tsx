cfb9aad9f8cffb60c40b4fce060d20616a35c693
lontonggg <zadareyhan@gmail.com>
2024-07-12 15:37:54 +0700

    fix: Fix some design in angkat isu page

--- src/components/angkat-isu/MainPoster.tsx
+++ src/components/angkat-isu/MainPoster.tsx
@@ -9,12 +9,12 @@ export const MainPoster = () => {
       <h1 className='text-4xl md:text-5xl xl:text-6xl text-center font-bold text-primary'>Angkat Isu</h1>
       <div className='grid grid-cols-1 md:grid-cols-2 md:pt-20'>
         <div className='hidden md:flex flex-col items-center justify-center p-20'>
-          <Image src='/angkat-isu/main-poster.png' width={500} height={500} alt='main-poster'/>
+          <Image src='/angkat-isu/main-poster.png' width={400} height={400} alt='main-poster'/>
         </div>
-        <div className='flex flex-col items-center justify-center md:px-10 gap-6 pt-8 md:pt-0'>
-          <h1 className='text-3xl font-bold text-center'>
+        <div className='flex flex-col items-center justify-center gap-6 pt-8 md:pt-0'>
+          <h1 className='text-4xl font-bold text-center leading-normal'>
             Cari tau isu terkini dan bantu{' '}
-            <div className='overflow-hidden h-10 relative'>
+            <div className='overflow-hidden h-10 relative md:px-10'>
               <span className='text-primary absolute left-0 right-0' style={{ textAlign: 'center' }}>
                 <h1 className='text-black'><span className='text-primary'>SWA</span>RA-<span className='text-primary'>kan!</span></h1>
               </span>
