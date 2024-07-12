c6fc731f46903cfb73977279ca877b78046fe461
lontonggg <zadareyhan@gmail.com>
2024-07-13 04:23:40 +0700

    fix: Fix next config for production

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'http',
-          hostname: '172.20.10.2',
-          port: '8000',
-          pathname: '/media/images/**',
-        },
-      ],
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: process.env.NEXT_PUBLIC_API.replace('https://', ''),
+        pathname: '/media/images/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
